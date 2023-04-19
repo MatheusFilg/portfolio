5c86b14bba515e6dba054e3a098b294e4081ad89
Matheus F <matheus_falmeida@hotmail.com>
2023-04-19 10:48:12 -0300

    Atualizando Projetos

--- src/pages/Projects/index.tsx
+++ src/pages/Projects/index.tsx
@@ -9,32 +9,35 @@ export function Project() {
     <ProjectContainer>
       <ImgContainer>
         <div>
-          <h3>Calculadora IMC</h3>
+          <h3>Coffee Delivery</h3>
           <img src={projeto1} alt=""></img>
           <span>
-            <NavLink to="https://github.com/MatheusFilg/IMC" target="_blank">
+            <NavLink
+              to="https://github.com/MatheusFilg/-desafio02-CoffeeDelivery-Rocketseat"
+              target="_blank"
+            >
               <button>Github</button>
             </NavLink>
-            <NavLink to="https://matheusf-imc.netlify.app" target="_blank">
+            <NavLink
+              to="https://matheusf-coffeedelivery.netlify.app"
+              target="_blank"
+            >
               <button>Site</button>
             </NavLink>
           </span>
         </div>
 
         <div>
-          <h3>Rede Social</h3>
+          <h3>Timer</h3>
           <img src={projeto2} alt=""></img>
           <span>
             <NavLink
-              to="https://github.com/MatheusFilg/mod01-react-rocketseat"
+              to="https://github.com/MatheusFilg/mod02-Timer-Rocketseat"
               target="_blank"
             >
               <button>Github</button>
             </NavLink>
-            <NavLink
-              to="https://matheusf-redesocial.netlify.app"
-              target="_blank"
-            >
+            <NavLink to="https://matheusf-timer.netlify.app/" target="_blank">
               <button>Site</button>
             </NavLink>
           </span>
